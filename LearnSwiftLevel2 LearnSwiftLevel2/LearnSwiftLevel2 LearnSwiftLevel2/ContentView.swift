dfd80c3ddfcae5480d724942feb688a2d56d3c1b
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-11 07:01:39 +0200

    Commit with the initial project

---
+++ LearnSwiftLevel2 LearnSwiftLevel2/LearnSwiftLevel2 LearnSwiftLevel2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnSwiftLevel2 LearnSwiftLevel2
+//
+//  Created by Valentin Fernandez on 11/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnSwiftLevel2 LearnSwiftLevel2/LearnSwiftLevel2 LearnSwiftLevel2/LearnSwiftLevel2_LearnSwiftLevel2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftLevel2_LearnSwiftLevel2App.swift
+//  LearnSwiftLevel2 LearnSwiftLevel2
+//
+//  Created by Valentin Fernandez on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftLevel2_LearnSwiftLevel2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
