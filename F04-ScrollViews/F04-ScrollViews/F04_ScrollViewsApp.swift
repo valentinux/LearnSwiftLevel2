cc3b1f71584886011e60bd74eb03a6eebec0daad
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-11 08:04:58 +0200

    Commit first lesson - the card view design

---
+++ F04-ScrollViews/F04-ScrollViews/F04_ScrollViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F04_ScrollViewsApp.swift
+//  F04-ScrollViews
+//
+//  Created by Valentin Fernandez on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct F04_ScrollViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
