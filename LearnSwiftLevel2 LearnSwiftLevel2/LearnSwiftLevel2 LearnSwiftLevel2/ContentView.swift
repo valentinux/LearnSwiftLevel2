57fd257c1ec8e26e50b4826a9b3b3be07bb6063a
Valentinux <107010509+valentinux@users.noreply.github.com>
2024-08-17 15:02:53 +0200

    Commit to add new repository (practiceURLSession)

--- LearnSwiftLevel2 LearnSwiftLevel2/LearnSwiftLevel2 LearnSwiftLevel2/ContentView.swift
+++ LearnSwiftLevel2 LearnSwiftLevel2/LearnSwiftLevel2 LearnSwiftLevel2/ContentView.swift
@@ -1,24 +1,53 @@
 //
-//  ContentView.swift
-//  LearnSwiftLevel2 LearnSwiftLevel2
+//  CardView.swift
+//  F04-ScrollViews
 //
 //  Created by Valentin Fernandez on 11/8/24.
 //
-
 import SwiftUI
 
-struct ContentView: View {
+struct CardView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("swift5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            HStack {
+                VStack(alignment: .leading){
+                    
+                    Text("Curso de swift 5")
+                        .font(.largeTitle)
+                        .foregroundColor(.secondary)
+                    
+                    Text("Aprende el lenguaje de programación de Apple desde 0")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    
+                    Text("Nivel: Fácil")
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                }
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+        .cornerRadius(16)
+        .overlay(
+            RoundedRectangle(cornerRadius: 16)
+                .stroke(Color(.sRGB, red: 140/255, green: 140/255, blue: 140/255, opacity: 0.6), lineWidth: 3))
+        
+        .padding([.top, .horizontal])
+        
     }
-}
+    
+    
+     }
+
 
 #Preview {
-    ContentView()
+    CardView()
 }
